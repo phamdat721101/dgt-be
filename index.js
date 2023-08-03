00286a42aeb65f1e51857d0f5afe62530c852312
MicheMichel <michel@vmi1108572.contaboserver.net>
2023-08-03 05:32:02 +0200

    test local api

--- index.js
+++ index.js
@@ -2,10 +2,34 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const axios = require('axios');
+
+// const fetch = require('node-fetch');
+
+// async function getData() {
+//     const url = 'http://localhost:4001/v1/status';
+//     const response = await fetch(url);
+//     const jsonResponse = await response.json();
+//     console.log("PQD: ", jsonResponse);
+// } 
+  
+// getData();
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+    let resp = axios.get('http://localhost:4001/v1/status')
+    .then(response => {
+        console.log("PQD: ",response.data);
+        return response.data
+    })
+    .catch(error => {
+        console.log(error);
+    });
+    res.json({
+        code: 0,
+        data: resp
+    })
 })
 
 app.listen(process.env.PORT || 3000);
